b30524a9d966313800941273f5b11478971adfa3
Gustavo Vásquez <47996439+gusvas26@users.noreply.github.com>
2019-09-06 17:12:16 -0500

    Add files via upload
    Programa Basico en C# de operaciones aritmeticas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+
+            Console.WriteLine("Introduzca un numero: ");
+            int x = Convert.ToInt32(Console.ReadLine());  //convertir tipo string a entero
+
+            Console.WriteLine("\nIntroduzca otro numero: ");
+            int y = Convert.ToInt32(Console.ReadLine());  //convertir tipo string a entero
+
+            int suma = x + y;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\nLa suma de ambos numeros es: " + suma);
+
+            Console.ReadKey();
+        }
+    }
+}
